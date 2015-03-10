1607123bc4ec57a650af0f6a49aaea1f2556ba5f
wonderboymusic <wonderboymusic@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-10 19:27:34 +0000

    update tag
    git-svn-id: https://plugins.svn.wordpress.org/audio-video-bonus-pack/tags/0.2@1109713 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitc96dd2ff95378caab003b936ab666614::getLoader();
+return ComposerAutoloaderInitc5a3488b5c950cdf6231dc08023a4713::getLoader();
